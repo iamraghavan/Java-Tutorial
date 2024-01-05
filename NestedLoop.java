14599c7323b5e0ae25ef9e9fd334e7df108a79e2
iamraghavan <developer.raghavan@gmail.com>
2024-01-05 11:33:25 +0530

    Java 4 Commit

---
+++ NestedLoop.java
@@ -0,0 +1,15 @@
+public class NestedLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 3 ; i++) {
+
+            for(int j = 0; j<3; j++){
+                System.out.println(i + "  " + j);
+            }
+
+        }
+
+    }
+
+}
